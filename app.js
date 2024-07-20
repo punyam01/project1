b58872c7d0dece28149e4b92ccebb0d2d5118d8d
punyam <punyamkumarsingh@gmail.com>
2024-07-20 11:55:16 +0530

    setting up env files

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 
 const express = require('express')
 const app = express ();
+require('dotenv').config()
 
-const PORT = 8000;
+// const PORT = 8000;
 
 // adding routes fxn 
 const userroutes = require("./routes/user.routes");
@@ -12,8 +13,8 @@ app.use("/api/v1/users" , userroutes);
 
 const start = async  () => {
   try {
-        await app.listen(PORT, () => {
-      console.log(`${PORT} is listening`);
+        await app.listen(process.env.PORT, () => {
+      console.log(`${process.env.PORT} is listening`);
     })
   } catch (error) {
     console.log(`port connection error ${error}`);
